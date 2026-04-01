9bebf69f36d93c949016e581a461e868d9566593
Wang.Y <294961525@qq.com>
2026-04-01 21:42:16 +0800

    260401: fix get_tokens_and_segments

--- block/bert.py
+++ block/bert.py
@@ -9,7 +9,7 @@ def get_tokens_and_segments(tokens_a, tokens_b=None):
     segments = [0] * (len(tokens_a) + 2)
     if tokens_b is not None:
         tokens += tokens_b + ['<sep>']
-        segments += [1] * (len(tokens_b) + 2)
+        segments += [1] * (len(tokens_b) + 1)
     return tokens, segments
 
 
